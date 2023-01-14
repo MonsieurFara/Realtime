a24fef7068166196ef0e999cc6ef286113c3135f
MonsieurFara <95968739+MonsieurFara@users.noreply.github.com>
2023-01-14 12:20:38 +0100

    Initial commit

---
+++ server/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import config from "config";
+import logger from "./utils/logger";
+
+
+
+import socket from "./socket";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials: true,
+  },
+});
+
+app.get("/", (_, res) =>
+  res.send(`Server is up and running version `)
+);
+
+httpServer.listen(port, host, () => {
+  logger.info(`🚀 Server version is listening 🚀`);
+  logger.info(`http://${host}:${port}`);
+
+  socket({ io });
+});
